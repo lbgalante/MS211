5c94192799b33f7521c452f3247ec906d4cedf95
Tiago Chaves <tiagoloureirochaves@gmail.com>
2018-09-30 14:40:11 -0300

    Inicializar com um loop apenas

--- projeto1/exercicio3/projeto1-3-b.py
+++ projeto1/exercicio3/projeto1-3-b.py
@@ -4,16 +4,16 @@ from numpy import dot
 def diagonals(A):
   n = len(A)
   a = [0.0 for i in range(n-1)] # lower diagonal (i-j==1)
-  b = [0.0 for i in range(n)] # main diagonal (i==j)
+  b = [0.0 for i in range(n)]   # main  diagonal  (i==j)
   c = [0.0 for i in range(n-1)] # upper diagonal (j-i==1)
-  for i in range(n):
-    for j in range(n):
-      if i-j == 1.0:
-        a[i-1] = A[i][j] # a starts with a_2, not a_1
-      elif i == j:
-        b[i] = A[i][j]
-      elif j - i == 1.0:
-        c[i] = A[i][j]
+  c[0] = A[0][1]
+  b[0] = A[0][0]
+  for i in range(1, n-1):
+    a[i-1] = A[i][i-1] # a starts with a_2, not a_1
+    b[i] = A[i][i]
+    c[i] = A[i][i+1]
+  a[n-2] = A[n-1][n-2]
+  b[n-1] = A[n-1][n-1]
   return (a, b, c)
 
 def tridiagonal_lu(A):
@@ -22,13 +22,11 @@ def tridiagonal_lu(A):
     L = [[(1.0 if i == j else 0.0) for i in range(n)] for j in range(n)]
     U = [[(1.0 if i == j else 0.0) for i in range(n)] for j in range(n)]
     for i in range(n-1):
-      for j in range(n):
-        if j - i == 1.0:
-          U[i][j] = c[i]
+      U[i][i+1] = c[i]
     
     U[1][1] = b[1]
     for k in range(1, n):
-      L[k][k-1] = a[k-1]/U[k-1][k-1] # we use a[k-1] instead of a[k] since a starts at a_2, not a_1
+      L[k][k-1] = a[k-1]/U[k-1][k-1] # we use a[k-1] instead of a[k] since a starts at a_2
       U[k][k] = b[k] - L[k][k-1]*c[k-1]
     
     return (L, U)
